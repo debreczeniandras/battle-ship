d824cd21e8bf29c48c5db3106cbc29bbc6b23860
Andras Debreczeni <andras.debreczeni@db-n.com>
2020-06-23 14:09:32 +0200

    add delete

--- tests/Controller/BattleControllerTest.php
+++ tests/Controller/BattleControllerTest.php
@@ -4,20 +4,20 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * @testdox Battle Controller
+ */
 class BattleControllerTest extends WebTestCase
 {
     /**
-     * @dataProvider provideOptionsForDimensions
-     *
-     * @param $payload
-     * @param $expWidth
-     * @param $expHeight
+     * @testdox      If set options method returns a correct battle object
+     * @return string|null
      */
-    public function testSetOptionsReturnsCorrectDimensions($payload, $expWidth, $expHeight)
+    public function testSetOptions(): string
     {
         $client = static::createClient();
-        $client->request('POST', '/api/v1/battles', [], [], ['CONTENT_TYPE' => 'application/json'], $payload);
-    
+        $client->request('POST', '/api/v1/battles', [], [], ['CONTENT_TYPE' => 'application/json'], '{"width": 10, "height": 10}');
+        
         $response = $client->getResponse();
         
         $this->assertEquals(201, $response->getStatusCode());
@@ -27,17 +27,48 @@ class BattleControllerTest extends WebTestCase
         $this->assertArrayHasKey('id', $data);
         $this->assertNotEmpty($data['id']);
         $this->assertArrayHasKey('options', $data);
-        $this->assertEquals($expWidth, $data['options']['width']);
-        $this->assertEquals($expHeight, $data['options']['height']);
+        $this->assertEquals(10, $data['options']['width']);
+        $this->assertEquals(10, $data['options']['height']);
         $this->assertArrayHasKey('state', $data);
         $this->assertEquals('ready', $data['state']);
+        
+        return $response->headers->get('Location');
     }
     
-    public function provideOptionsForDimensions()
+    /**
+     * @depends testSetOptions
+     * @testdox Test if a Battle object is returned.
+     *
+     * @param $url
+     */
+    public function testGetBattle($url)
     {
-        return [
-            ['{}', 8, 8],
-            ['{"width": 10, "height": 10}', 10, 10],
-        ];
+        $client = static::createClient();
+        $client->request('GET', $url);
+        
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode());
+    
+        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $data);
+        $this->assertNotEmpty($data['id']);
+        $this->assertArrayHasKey('options', $data);
+        
+        return $url;
+    }
+    
+    /**
+     * @depends testGetBattle
+     * @testdox Test if a Battle is deleted.
+     *
+     * @param $url
+     */
+    public function testDeleteBattle($url)
+    {
+        $client = static::createClient();
+        $client->request('DELETE', $url);
+        
+        $response = $client->getResponse();
+        $this->assertEquals(204, $response->getStatusCode());
     }
 }
